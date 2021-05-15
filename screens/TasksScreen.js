ea2a226c98bd188ebd339e53a3b4b3760ec6b826
Avi Khemani <akhemani@stanford.edu>
2021-05-15 15:45:22 -0700

    changed from green to black for completion budddddy

--- screens/TasksScreen.js
+++ screens/TasksScreen.js
@@ -40,8 +40,8 @@ export default class TasksScreen extends React.Component {
       <View style={{ backgroundColor: '#FAFAFA', flex: 1 }}>
           <View style={{ flexWrap: 'wrap', flexDirection: 'row', justifyContent: 'space-around'}}>
             {this.props.tasks.map(task =>
-              <View key={task.name} style={{...Styling.containers.card, marginTop: 24, alignItems: 'center', justifyContent: 'space-around', width: '40%', height: 150, backgroundColor: this.alreadyCompleted(task) ? '#75DAAD' : 'white' }}>
-                <Text style={{...Styling.text.bodyLarge, textAlign: 'center' }}>
+              <View key={task.name} style={{...Styling.containers.card, marginTop: 24, alignItems: 'center', justifyContent: 'space-around', width: '40%', height: 150, backgroundColor: this.alreadyCompleted(task) ? Styling.colors.primary : 'white' }}>
+                <Text style={{...Styling.text.bodyLarge, textAlign: 'center', color: this.alreadyCompleted(task) ? 'white' : Styling.colors.primary }}>
                   {task.name}
                 </Text>
                 { !this.alreadyCompleted(task) ?
@@ -50,7 +50,7 @@ export default class TasksScreen extends React.Component {
                 </TouchableOpacity>
                 :
                 <TouchableOpacity style={{height: 40, justifyContent: 'center'}} onPress={() => this.props.handleUndoTask(task.name)}>
-                  <Text style={{ color: 'red', fontSize: 16 }}>Undo</Text>
+                  <Text style={{...Styling.text.label, color: 'red', fontSize: 16 }}>Undo</Text>
                 </TouchableOpacity>
                 }
               </View>
